8c909673247fade45f7e6b49b3cff422be05e83d
AidanJP <aidanpendlebury@gmail.com>
2021-02-02 22:50:37 +0000

    Add error handling.

--- main.py
+++ main.py
@@ -2,30 +2,35 @@ from bs4 import BeautifulSoup
 import requests
 
 CURRENT_YEAR = 2021
+years_to_collect = 42
 
-for year in range(CURRENT_YEAR - 20, CURRENT_YEAR + 1):
+for year in range(CURRENT_YEAR - years_to_collect, CURRENT_YEAR + 1):
     print(f"Fetching data for {year}")
     players = []
 
     # Fetch driving stats table for the current year
     response = requests.get(url=f"https://www.pgatour.com/stats/stat.101.y{year}.html")
     soup = BeautifulSoup(response.text, "html.parser")
-    rows = soup.find("table", {"id": "statsTable"}).find_all("tr")
 
-    # Extract details for each player in the driving stats table
-    for row in rows[1:]:
-        details = row.find_all("td")
-        position = details[0].getText().strip()
-        name = details[2].getText().strip()
-        avg_dist = details[4].getText().strip()
-        players.append(f"{position},{name},{avg_dist}")
+    try:
+        rows = soup.find("table", {"id": "statsTable"}).find_all("tr")
 
-    # Find tour avg for the year
-    tour_average = soup.select_one(selector=".tour-average").find("span").getText().strip()
+        # Extract details for each player in the driving stats table
+        for row in rows[1:]:
+            details = row.find_all("td")
+            position = details[0].getText().strip()
+            name = details[2].getText().strip()
+            avg_dist = details[4].getText().strip()
+            players.append(f"{position},{name},{avg_dist}")
 
-    # Write to csv file
-    with open(f"driving_stats/stats_{year}_avg_{tour_average}.csv", "w") as csv_file:
-        for player in players:
-            csv_file.write(player)
-            csv_file.write("\n")
+        # Find tour avg for the year
+        tour_average = soup.select_one(selector=".tour-average").find("span").getText().strip()
+
+        # Write to csv file
+        with open(f"driving_stats/stats_{year}_avg_{tour_average}.csv", "w") as csv_file:
+            for player in players:
+                csv_file.write(player)
+                csv_file.write("\n")
+    except AttributeError:
+        print(f"Couldn't get data for {year}")
 
